ca5e16457763c55a2a08b14719fa5866fcdf29ee
Bersain25 <125053434@upq.edu.mx>
2026-09-18 17:08:08 -0600

    Practica1_Bersain

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+<link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+
+<div>
+    <!-- las etiquetas h vienen en heading, que significa encabezado o titulo. -->
+    <h1> Titulo Principal </h1>
+    <h2> Subtitulo </h2>
+    <h3> Subtitulo secundario </h3>
+    <h4> Titulo </h4>
+    <h5> Titulo </h5>
+    <h6> Titulo </h6>
+
+    <p> Estoy aprendiendo desarrollo web con HTML, CSS y PHP.</p>
+</div>
+
+    <div>
+        <h2> Mi seccion </h2>
+
+        <p> Este es el contenido de esta seccion </p>
+    </div>
+</body>
+</html>
